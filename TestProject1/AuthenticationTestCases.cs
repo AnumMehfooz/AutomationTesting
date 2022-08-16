fd4e2e2b2a379d681620c3e01d29960f3ea27f21
Anum Mehfooz <anum.mehfooz@letsdreambeyond.com>
2022-08-16 22:46:06 +0500

    Test project

---
+++ TestProject1/AuthenticationTestCases.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    internal class AuthenticationTestCases
+    {
+        public void Login(IWebDriver driver)
+        {
+            driver.Url = "https://localhost:44307/";
+            driver.FindElement(By.Id("Email")).SendKeys("Owner1@gmail.com");
+            driver.FindElement(By.Id("Password")).SendKeys("123456");
+            driver.FindElement(By.CssSelector(".col-xs-4")).Click();
+            driver.FindElement(By.XPath(".//*[@id='introjs-dontShowAgain']")).Click();
+            
+        }
+
+    }
+}
---
+++ TestProject1/TestBase.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    public class TestBase
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();       // Launches browser
+            driver.Manage().Window.Maximize(); //Maximizes browser
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10); //Set locator timeout to be 20s at max
+        }
+
+        //[OneTimeTearDown]
+        //public void cleanUp()
+        //{
+        //    driver.Quit();
+        //}
+    }
+}
